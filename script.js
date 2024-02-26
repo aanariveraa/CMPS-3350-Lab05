4f0b78feb5eacc6b6b16b5d28c5c8684a2c757fa
andrea-s7 <133468366+andrea-s7@users.noreply.github.com>
2024-02-26 11:44:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import { updateGround, setupGround } from "./ground.js"
+import { updateCat, setupCat, getCatRect, setCatLose  } from "./cat.js"
+import { updateWater, setupWater, getWaterRects} from "./water.js"
 
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
@@ -25,13 +27,27 @@ function update(time) {
 
   updateGround(delta, speedScale)
   updateSpeedScale(delta)
+  updateCat(delta, speedScale)
+  updateWater(delta, speedScale)
   updateScore(delta)
   
 
   lastTime = time
   window.requestAnimationFrame(update)
 }
+function checkLose() { 
+  const catRect = getCatRect()
+  return getWaterRects().some(rect => isCollision(rect, catRect))
+}
 
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  )
+}
 
 function updateSpeedScale(delta) {
   speedScale += delta * SPEED_SCALE_INCREASE
@@ -47,9 +63,20 @@ function handleStart() {
   speedScale = 1
   score = 0
   setupGround()
+  setupCat()
+  setupWater()
   startScreenElem.classList.add("hide")
   window.requestAnimationFrame(update)
 }
+
+function handleLose() {
+  setCatLose()
+  setTimeout(() => {
+    document.addEventListener("keydown", handleStart, { once: true })
+    startScreenElem.classList.remove("hide")
+  }, 100)
+}
+
 
 function setPixelToWorldScale() { /* SCALES display to screen size*/
   let worldToPixelScale
